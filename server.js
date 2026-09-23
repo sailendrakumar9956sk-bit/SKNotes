7287a7279925e726dab143d1ca02e8d1bedea2c3
sailendrakumar9956sk-bit <sailendrakumar9956sk@gmail.com>
2026-09-23 12:22:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ ${text.slice(0, 30000)}
 `;
 
     const completion = await ai.chat.completions.create({
-      model: "llama-3.1-8b-instant",
+      model: "openai/gpt-oss-20b",
       messages: [
         {
           role: "user",
