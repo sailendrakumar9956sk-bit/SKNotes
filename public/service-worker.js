cb05df4723e6cb7f6416378ac256763f43853654
sailendrakumar9956sk-bit <sailendrakumar9956sk@gmail.com>
2026-09-23 10:15:41 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "sknotes-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sknotes-icon-192.png",
+  "/sknotes-icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
